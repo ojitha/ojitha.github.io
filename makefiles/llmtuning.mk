df0a87653ff897cd5150832570917e9e0b91d139
Ojitha Hewa Kumanayaka <ojithak@gmail.com>
2026-05-05 23:25:22 +1000

    added Gemma 4 on AMD

--- makefiles/llmtuning.mk
+++ makefiles/llmtuning.mk
@@ -1,5 +1,5 @@
 LLMTUNINGBlogsDir := ../LLMTuning/blogs
-LLMTUNINGBlogsSources := 2026-03-07-ContainerRocm
+LLMTUNINGBlogsSources := 2026-03-07-ContainerRocm 2026-05-05-Gemma4
 
 md_targets += $(foreach wrd,$(LLMTUNINGBlogsSources),$(DRAFTS_DIR)/$(wrd).md)
 asset_targets += $(foreach wrd,$(LLMTUNINGBlogsSources),$(ASSETS_DIR)/$(wrd))
