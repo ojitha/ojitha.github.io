df0791c323a181f520e844afbbf8af7544db74a6
Ojitha Hewa Kumanayaka <ojithak@gmail.com>
2026-09-16 23:38:02 +1000

    Added Bedrock Claude Second pard

--- makefiles/bedrock.mk
+++ makefiles/bedrock.mk
@@ -1,6 +1,7 @@
 BedrockBlogsDir := ../learn-bedrock/blogs
 BedrockBlogsSources := 2025-08-29-BedrockLangGraph \
-	2026-09-13-BedrocClaude_1
+	2026-09-13-BedrocClaude_1 \
+	2026-09-16-BedrockClaude_2
 
 md_targets += $(foreach wrd,$(BedrockBlogsSources),$(DRAFTS_DIR)/$(wrd).md)
 asset_targets += $(foreach wrd,$(BedrockBlogsSources),$(ASSETS_DIR)/$(wrd))
